e9249458b958f9ae1373d2ce895ddd22a23f6e61
RickieAustin114 <rcya996001@gmail.com>
2024-10-23 15:38:00 -0400

    edited

--- frontend/src/screens/MenuScreen.tsx
+++ frontend/src/screens/MenuScreen.tsx
@@ -1,12 +1,14 @@
 // src/screens/MenuScreen.tsx
 import React from 'react';
-import { View, Button, StyleSheet } from 'react-native';
+import { View, Button, StyleSheet, Text } from 'react-native';
 
 const MenuScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Button title="Add Patient" onPress={() => navigation.navigate('AddPatient')} />
+      <Text />
       <Button title="Add Record" onPress={() => navigation.navigate('AddRecord')} />
+      <Text />
       <Button title="List Patients" onPress={() => navigation.navigate('ListPatients')} />
     </View>
   );
